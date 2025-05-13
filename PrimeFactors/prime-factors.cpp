f092dbe0e471e742ccd6cbcda85463e795cd31c4
eunho.lee <eno99.lee@samsung.com>
2025-05-13 14:00:30 +0900

    [refactoring] 3

--- PrimeFactors/prime-factors.cpp
+++ PrimeFactors/prime-factors.cpp
@@ -6,10 +6,10 @@ public:
 	std::vector<int> of(int number) {
 		std::vector<int> result = {};
 		if (number == 2) {
-			result.push_back(2);
+			result.push_back(number);
 		}
 		if (number == 3) {
-			result.push_back(3);
+			result.push_back(number);
 		}
 		return result;
 	}
