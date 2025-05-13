572a59eb5f80dc5c3ef44d73773a9ed2ec39be08
eunho.lee <eno99.lee@samsung.com>
2025-05-13 13:59:00 +0900

    [feature] 2

--- PrimeFactors/prime-factors.cpp
+++ PrimeFactors/prime-factors.cpp
@@ -5,6 +5,9 @@ class PrimeFactor {
 public:
 	std::vector<int> of(int number) {
 		std::vector<int> result = {};
+		if (number == 2) {
+			result.push_back(2);
+		}
 		return result;
 	}
 };
--- PrimeFactors/prime-factors_test.cpp
+++ PrimeFactors/prime-factors_test.cpp
@@ -14,3 +14,8 @@ TEST_F(PrimeFixture, Of1) {
 	vector<int> expected = {};
 	EXPECT_EQ(expected, prime_factor.of(1));
 }
+
+TEST_F(PrimeFixture, Of2) {
+	vector<int> expected = {2};
+	EXPECT_EQ(expected, prime_factor.of(2));
+}
